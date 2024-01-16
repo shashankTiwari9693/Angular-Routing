95c61bc23f96c5637cb33b2af2b634a68990e9a6
Shashank Tiwari <1928260@kiit.ac.in>
2024-01-16 23:37:49 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,79 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Injectable,NgModule } from '@angular/core';
 
-const routes: Routes = [];
+//import { RouterModule, Routes } from '@angular/router';
+
+import {Title} from "@angular/platform-browser";
+
+import{
+
+  Routes,
+
+  RouterModule,
+
+  TitleStrategy,
+
+  RouterStateSnapshot,
+
+  RouterState,
+
+} from "@angular/router";
+
+import { BarComponent } from './bar/bar.component';
+
+import { FooComponent } from './foo/foo.component';
+
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+ 
+
+const routes: Routes = [
+
+  {path :"foo", title: "Foo", component: FooComponent},
+
+  {path:"bar", "title": "Bar", component:BarComponent},
+
+  {path:"", redirectTo : "/foo", pathMatch: "full"},
+
+  {path:"**", component: PageNotFoundComponent},
+
+];
+
+ 
+
+ 
+
+@Injectable()
+
+export class TemplatePageTitleStrategy extends TitleStrategy{
+
+  constructor(private readonly title: Title){
+
+    super();
+
+  }
+
+  override updateTitle(snapshot: RouterStateSnapshot){
+
+    const title = this.buildTitle(snapshot);
+
+    if(title !== undefined){
+
+      this.title.setTitle(`App | ${title}`);
+
+    }
+
+  }
+
+}
 
 @NgModule({
+
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+
+  exports: [RouterModule],
+
+  providers: [{provide : TitleStrategy, useClass: TemplatePageTitleStrategy}],
+
 })
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooComponent } from './foo/foo.component';
+import { BarComponent } from './bar/bar.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooComponent,
+    BarComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
